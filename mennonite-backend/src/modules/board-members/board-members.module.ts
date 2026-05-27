11cedd4dd8c271a1c79882319bd56de4b96cce87
DavidZelaya12 <davidzelaya06.isj@gmail.com>
2026-05-27 16:11:08 -0600

    Fix in controllers

--- mennonite-backend/src/modules/board-members/board-members.module.ts
+++ mennonite-backend/src/modules/board-members/board-members.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BoardMembersController } from './board-members.controller';
-import { BoardsMembersController } from './boards-members.controller';
 import { BoardMembersService } from './board-members.service';
 
 @Module({
-  controllers: [BoardMembersController, BoardsMembersController],
+  controllers: [BoardMembersController],
   providers: [BoardMembersService],
 })
 export class BoardMembersModule {}
