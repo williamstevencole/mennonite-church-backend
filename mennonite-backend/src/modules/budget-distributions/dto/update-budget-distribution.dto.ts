fc492e92c89610ee8c827b36ec0eef3ebde14f95
Andrea Quin <andrea.quin123@gmail.com>
2026-05-28 12:51:57 -0600

    ran npm run lint

--- mennonite-backend/src/modules/budget-distributions/dto/update-budget-distribution.dto.ts
+++ mennonite-backend/src/modules/budget-distributions/dto/update-budget-distribution.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBudgetDistributionDto } from './create-budget-distribution.dto';
 
-export class UpdateBudgetDistributionDto extends PartialType(CreateBudgetDistributionDto) {}
+export class UpdateBudgetDistributionDto extends PartialType(
+  CreateBudgetDistributionDto,
+) {}
